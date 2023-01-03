2667256a1a2c380d48fb5cb4ef6e72b9f3b67795
Sebastian <Duerr.sebastian@gmail.com>
2023-01-02 20:02:36 -0800

    build successful

---
+++ out/_next/static/chunks/pages/Layout-eeae51a3361a8b4f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[511],{1949:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Layout",function(){return t(2674)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=1949)}),_N_E=n.O()}]);
